1147fa541f2ad2afbfcdf25ba25adab7eb6159b1
Himanshu Verma <vhimanshu0817@gmail.com>
2026-05-14 01:52:27 +0530

    Delete server directory

--- server/middleware/roleAuth.js
+++
@@ -1,9 +0,0 @@
-const authorize=(...roles)=>(req,res,next)=>{
-    if(!roles.includes(req.user.role)){
-      return res.status(403).json({success:false, message:"Access denied"});
-    }
-    next();
-
-}
-
-module.exports=authorize;
